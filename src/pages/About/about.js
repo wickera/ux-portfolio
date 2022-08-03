08cea9ff3825cb0983320aa46d2e2201de08f666
wickera <wickera74@gmail.com>
2022-08-03 12:59:49 -0400

    clean up styles

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -8,19 +8,19 @@ export default function About() {
     <img src={miafair} alt='About' className='about__image--img' />
    </div>
 
-   <div>
-    <h2>I'm Mia, Nice to meet you.</h2>
-    <p>
+   <div className='about__desc'>
+    <h2 className='about__desc--title'>I'm Mia, Nice to meet you.</h2>
+    <p className='about__desc--text'>
      Strategic thinking is my forte, and I enjoy observing and analyzing
      individuals' and organizations' needs and desires. All people have worth,
      and organizations exist to support or impove their worth.
     </p>
-    <p>
+    <p className='about__desc--text'>
      I take great pride in my desire to help others. One of my primary
      contributions to organizations in the past is the development of effective
      responses to situations.
     </p>
-    <p>
+    <p className='about__desc--text'>
      The utilization of multiple resources, the collaboration of talented,
      innovative teams, and the ability to take advantage of every opportunity
      posture me to create, create, create! I can't wait to demonstrate my
